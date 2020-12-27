327a9f973580152c19969b8fc7113268c05c6562
0entropie <76582928+0entropie@users.noreply.github.com>
2020-12-27 14:48:24 +0100

    errpp.py: Refactored, removed ugly api.

--- errpp.py
+++ errpp.py
@@ -1,221 +1,216 @@
-from abc import ABC, abstractmethod
+import abc
 import math
 
-
-class ValueWithError(ABC):
-
-    def __init__(self, value, error=0):
-        self.value = value
-        self.error = error              # absolute error
-
-    def get_percent_err(self):
-        if self.value == 0:
-            raise ZeroDivisionError(self)
-        return 100 * self.error/abs(self.value)
-
-    def get_ppm_err(self):
-        if self.value == 0:
-            raise ZeroDivisionError(self)
-        return 1e6 * self.error/abs(self.value)
-
-    def type_check(self, other):
-        if type(self) != type(other):
-            raise TypeError("Different error types: ", type(self), type(other))
-
-    def zeroDiv_check(self, other):
-        if other.value == 0 or abs(other.value) <= other.error:
-            raise ZeroDivisionError(other)
-
-    @abstractmethod
-    def __add__(self, other): pass
-
-    @abstractmethod
-    def __sub__(self, other): pass
-
-    @abstractmethod
-    def __mul__(self, other): pass
-
-    @abstractmethod
-    def __truediv__(self, other): pass
-
-    @abstractmethod
-    def __neg__(self): pass
-
-    def __repr__(self):
-        try: rel_error = "[{0:.3f}%]".format(self.get_percent_err())
-        except ZeroDivisionError: rel_error = ""
-        return "{0:.3f} \u00B1 {1:.3f} {2}".format(self.value, self.error, rel_error)
-
-    def __str__(self):
-        return self.__repr__()
-
-class StatisticalError(ValueWithError):
-
-    # q = x+y , dq = sqrt( dx**2 + dy**2 )
-    def __add__(self, other):
-        self.type_check(other)
-        return StatisticalError(self.value + other.value, math.sqrt(self.error**2 + other.error**2))
-
-    # q = x-y , sqrt( dx**2 + dy**2 )
-    def __sub__(self, other):
-        self.type_check(other)
-        return StatisticalError(self.value - other.value, math.sqrt(self.error**2 + other.error**2))
-
-    # q = xy , dq = q * sqrt( (dx/x)**2 + (dy/y)**2 ) = sqrt( (dx*y)**2 + (dy*x)**2 )
-    def __mul__(self, other):
-        self.type_check(other)
-        return StatisticalError(self.value * other.value,
-                                math.sqrt((self.error * other.value)**2 + (other.error * self.value)**2))
-
-    # q = x/y , dq = q * sqrt( (dx/x)**2 + (dy/y)**2 ) = sqrt( (dx/y)**2 + (dy*x/(y*y))**2 )
-    def __truediv__(self, other):
-        self.type_check(other)
-        self.zeroDiv_check(other)
-        return StatisticalError(self.value / other.value,
-                                math.sqrt((self.error/other.value)**2 + (other.error*self.value/(other.value**2))**2))
-
-    def __neg__(self):
-        return StatisticalError(-self.value, self.error)
-
-class WorstCaseError(ValueWithError):
-
-    # q = x+y , dq = dx+dy
-    def __add__(self, other):
-        self.type_check(other)
-        return WorstCaseError(self.value + other.value, self.error + other.error)
-
-    # q = x-y , dq = dx+dy
-    def __sub__(self, other):
-        self.type_check(other)
-        return WorstCaseError(self.value - other.value, self.error + other.error)
-
-    # q = xy , dq = |q|*(dx/|x| + dy/|y|) = dx*|y| + dy*|x|
-    def __mul__(self, other):
-        self.type_check(other)
-        return WorstCaseError(self.value * other.value, self.error * abs(other.value) + other.error * abs(self.value))
-
-    # q = x/y , dq = |q|*(dx/|x| + dy/|y|) = dx/|y| + dy*|x|/|y*y|
-    def __truediv__(self, other):
-        self.type_check(other)
-        self.zeroDiv_check(other)
-        return WorstCaseError(self.value / other.value,
-                              self.error / abs(other.value) + other.error * abs(self.value) / (other.value**2))
-
-    def __neg__(self):
-        return WorstCaseError(-self.value, self.error)
-
-class ExtremeError(WorstCaseError):
-
-    # q = x+y , dq = dx+dy
-    def __add__(self, other):
-        self.type_check(other)
-        return ExtremeError(self.value + other.value, self.error + other.error)
-
-    # q = x-y , dq = dx+dy
-    def __sub__(self, other):
-        self.type_check(other)
-        return ExtremeError(self.value - other.value, self.error + other.error)
-
-    # q = xy , dq = |q|*(dx/|x| + dy/|y| + (dx/x)*(dy/y)) = dx*|y| + dy*|x| + dx*dy
-    def __mul__(self, other):
-        self.type_check(other)
-        return ExtremeError(self.value * other.value,
-                            self.error * abs(other.value) + other.error * abs(self.value) + self.error * other.error)
-
-    # q = x/y , q(1 + dq/|q|) = x(1 + dx/|x|) / (y(1 + dy/|y|)) ,, dq = (|x| + dx) / (|y| - dy) - |x/y|
-    def __truediv__(self, other):
-        self.type_check(other)
-        self.zeroDiv_check(other)
-        return ExtremeError(self.value / other.value,
-                            (abs(self.value) + self.error)/(abs(other.value) - other.error) - abs(self.value/other.value))
-
-    def __neg__(self):
-        return ExtremeError(-self.value, self.error)
-
-conversion_dict = {StatisticalError: "toStatisticalError", WorstCaseError: "toWorstCaseError", ExtremeError: "toExtremeError"}
-
-def mlt_percent(list_percentage, factor=100):
-    return [p * factor for p in list_percentage]
-
-def div_percent(list_percentage, factor=100):
-    return [p / factor for p in list_percentage]
-
-def abserr_to_relerr(list):
-    return [mlt_percent([abserr / val])[0] for val,abserr in list]
-
-def relerr_to_abserr(list):
-    return [val * div_percent([relerr])[0] for val,relerr in list]
-
-def abspp(list):
-    return math.sqrt(sum([abserr ** 2 for abserr in list]))
-
-def relpp(list):
-    return mlt_percent([math.sqrt(sum([relerr ** 2 for relerr in div_percent(list)]))])[0]
-
-def abspp_rel(res, list):
-    return abserr_to_relerr([(res, abspp(relerr_to_abserr(list)))])[0]
-
-def relpp_abs(res, list):
-    return relerr_to_abserr([(res, relpp(abserr_to_relerr(list)))])[0]
-
-
-
- #import math
-
 __percent_scale_factor = 100
+__ppm_scale_factor = 1_000_000
 
-#def decimal_to_percent(dec):
-#    if not isinstance(dec, float):
-#    raise TypeError("Only converting floats")
-    #    return dec * __percent_scale_factor
+def decimal_to_percent(dec):
+    return dec * __percent_scale_factor
+
+def decimal_to_ppm(dec):
+    return dec * __ppm_scale_factor
 
 def rel_to_abs_err(value, rel_err):
-    return value * rel_err / __percent_scale_factor
+    return value * rel_err
+
 
 def abs_to_rel_err(value, abs_err):
-    return value * rel_err / __percent_scale_factor
+    return abs_err / value
+
 
 def percent_to_decimal(per):
     return per / __percent_scale_factor
 
-# too generic as a function, especially with selectable factor
-#def mlt_percent(list_percentage, factor=100):
-    #return [p * factor for p in list_percentage]
+class ValueWithError:
+    def __init__(self, value, error):
+        self.value = value
+        if not isinstance(error, BaseError):
+            raise TypeError(f"Expected Error Object but got {type(error)}")
+        self.error = error  # Error Object not number
 
-# same as above
-#def div_percent(list_percentage, factor=100):
-    #return [p / factor for p in list_percentage]
+    def get_error(self):
+        return self.error
 
-#avoid builtin names for variables and shit
-#def abserr_to_relerr(val_err_list):
-    #return [decimal_to_percent(abserr / val) for val,abserr in val_err_list]
-#same as above
-#def relerr_to_abserr(val_err_list):
-    #return [val * percent_to_decimal(relerr) for val,relerr in val_err_list]
+    def __add__(self, other):
+        new_val = self.value + other.value
+        return ValueWithError(new_val, self.error.update_err_add(new_val, other.error))
 
-# ? standard deviation? Also builtin name again
-#def abspp(abserrs):
-    #return math.sqrt(sum([abserr ** 2 for abserr in abserrs]))
+    def __sub__(self, other):
+        new_val = self.value - other.value
+        return ValueWithError(new_val, self.error.update_err_sub(new_val, other.error))
 
-#def relpp(rel_errs):
-    # ugly as hell
-    # return mlt_percent([math.sqrt(sum([relerr ** 2 for relerr in div_percent(list)]))])[0]
-    # don't need a list, iterator is more performant
-    #rel_dec = (percent_to_decimal(per) for per in rel_errs)
-    #rel_squares = (rel_err ** 2 for rel_err in rel_dec)
-    #return decimal_to_percent(math.sqrt(sum(rel_squares)))
+    def __mul__(self, other):
+        new_val = self.value * other.value
+        return ValueWithError(new_val, self.error.update_err_mul(new_val, other.error))
+
+    def __truediv__(self, other):
+        if other.value == 0:
+            raise ZeroDivisionError("Attempt to divide by 0 Value")
+        new_val = self.value / other.value
+        return ValueWithError(new_val, self.error.update_err_div(new_val, other.error))
+
+    def __neg__(self):
+        return ValueWithError(-self.value, self.error)
+
+    def __repr__(self):
+        try:
+            rel_error = "[{0:.3f}%]".format(self.error.get_percent_err())
+        except ValueError:
+            rel_error = "--%"
+        return "{0:.3f} \u00B1 {1:.3f} {2}".format(self.value,
+                                                   self.error.abs_err,
+                                                   rel_error)
+
+    def __str__(self):
+        return self.__repr__()
+
+class BaseError(abc.ABC):
+    def __init__(self, abs_err, rel_err):
+        self.__abs_err = abs(abs_err)
+        if rel_err is not None:
+            self.__rel_err = abs(rel_err)
+        else:
+            self.__rel_err = None
+
+    @property
+    def abs_err(self):
+        return self.__abs_err
+
+    @property
+    def rel_err(self):
+        return self.__rel_err
+
+    def get_percent_err(self):
+        if self.__rel_err is not None:
+            return decimal_to_percent(self.__rel_err)
+        else:
+            raise ValueError("Relative Error is not defined")
+
+    def get_ppm_err(self):
+        if self.__rel_err is not None:
+            return decimal_to_ppm(self.__rel_err)
+        else:
+            raise ValueError("Relative Error is not defined")
+
+    def type_check(self, other):
+        if not isinstance(other, self.__class__):
+            raise TypeError("Different error types: ", type(self), type(other))
+
+    @abc.abstractmethod
+    def update_err_add(self, ref_val, other):
+        pass
+
+    @abc.abstractmethod
+    def update_err_sub(self, ref_val, other):
+        pass
+
+    @abc.abstractmethod
+    def update_err_mul(self, ref_val, other):
+        pass
+
+    @abc.abstractmethod
+    def update_err_div(self, ref_val, other):
+        pass
+
+class StatisticalError(BaseError):
+
+    # q = x+y , dq = sqrt( dx**2 + dy**2 )
+    def update_err_add(self, ref_val, other):
+        self.type_check(other)
+        new_abs_err = math.sqrt(self.abs_err**2 + other.abs_err**2)
+        if ref_val != 0:
+            new_rel_err = abs(new_abs_err / ref_val)
+        else:
+            new_rel_err = None
+
+        return StatisticalError(new_abs_err, new_rel_err)
+
+    # q = x-y , sqrt( dx**2 + dy**2 )
+    def update_err_sub(self, ref_val, other):
+        return self.update_err_add(ref_val, other)
+
+    # q = xy , dq = q * sqrt( (dx/x)**2 + (dy/y)**2 ) = sqrt( (dx*y)**2 + (dy*x)**2 )
+    def update_err_mul(self, ref_val, other):
+        self.type_check(other)
+
+        if self.rel_err is None or other.rel_err is None:
+            raise ValueError("Attempt to propagate invalid relative Error")
+
+        new_rel_error = math.sqrt(self.rel_err**2 + other.rel_err**2)
+        new_abs_error = abs(ref_val * new_rel_error)
+        return StatisticalError(new_abs_error, new_rel_error)
+
+    # q = x/y , dq = q * sqrt( (dx/x)**2 + (dy/y)**2 ) = sqrt( (dx/y)**2 + (dy*x/(y*y))**2 )
+    def update_err_div(self, ref_val, other):
+        return self.update_err_mul(ref_val, other)
+
+class WorstCaseError(BaseError):
+
+    # q = x+y , dq = dx+dy
+    def update_err_add(self, ref_val, other):
+        self.type_check(other)
+        new_abs_error = self.abs_err + other.abs_err
+
+        if ref_val != 0:
+            new_rel_error = abs(new_abs_error / ref_val)
+        else:
+            new_rel_error = None
+        return WorstCaseError(new_abs_error, new_rel_error)
+
+    # q = x-y , dq = dx+dy
+    def update_err_sub(self, ref_val, other):
+        return self.update_err_add(ref_val, other)
+
+    # q = xy , dq = |q|*(dx/|x| + dy/|y|) = dx*|y| + dy*|x|
+    def update_err_mul(self, ref_val, other):
+        self.type_check(other)
+
+        if self.rel_err is None or other.rel_err is None:
+            raise ValueError("Attempt to propagate invalid relative Error")
+
+        new_rel_error = self.rel_err + other.rel_err
+        new_abs_error = abs(new_rel_error * ref_val)
+        return WorstCaseError(new_abs_error, new_rel_error)
+
+    # q = x/y , dq = |q|*(dx/|x| + dy/|y|) = dx/|y| + dy*|x|/|y*y|
+    def update_err_div(self, ref_val, other):
+        return self.update_err_mul(ref_val, other)
 
 
-#def abspp_rel(res, rel_errs):
-    # write once, read never again... also builtin name again >:-[
-    # return abserr_to_relerr([(res, abspp(relerr_to_abserr(list)))])[0]
-    #abs_errs = relerr_to_abserr(rel_errs)
-    #abs_deviation = abspp(abs_errs)
-    #return decimal_to_percent(abs_deviation/res)
+class ExtremeError(BaseError):
 
-#def relpp_abs(res, abs_errs):
-    # same as above rly
-    # return relerr_to_abserr([(res, relpp(abserr_to_relerr(list)))])[0]
-    #rel_errs = abserr_to_relerr(abs_errs)
-    #rel_deviation = relpp(rel_erss)
-    #    return res * percent_to_decimal(rel_deviation)
+    # q = x+y , dq = dx+dy
+    def update_err_add(self, ref_val, other):
+        self.type_check(other)
+        new_abs_error = self.abs_err + other.abs_err
+
+        if ref_val != 0:
+            new_rel_error = abs(new_abs_error / ref_val)
+        else:
+            new_rel_error = None
+
+        return ExtremeError(new_abs_error, new_rel_error)
+
+    # q = x-y , dq = dx+dy
+    def update_err_sub(self, ref_val, other):
+        return self.update_err_add(ref_val, other)
+
+    # q = xy , dq = |q|*(dx/|x| + dy/|y| + (dx/x)*(dy/y)) = dx*|y| + dy*|x| + dx*dy
+    def update_err_mul(self, ref_val, other):
+        self.type_check(other)
+
+        if self.rel_err is None or other.rel_err is None:
+            raise ValueError("Attempt to propagate invalid relative Error")
+
+        new_rel_error = self.rel_err + other.rel_err + self.rel_err * other.rel_err
+        new_abs_error = abs(new_rel_error * ref_val)
+        return ExtremeError(new_abs_error, new_rel_error)
+
+    # q = x/y , q(1 + dq/|q|) = x(1 + dx/|x|) / (y(1 + dy/|y|)) ,, dq = (|x| + dx) / (|y| - dy) - |x/y|
+    def update_err_div(self, ref_val, other):
+        #self.type_check(other)
+        #self.zeroDiv_check(other)
+        #return ExtremeError(
+        #    self.value / other.value, (abs(self.value) + self.error) /
+        #    (abs(other.value) - other.error) - abs(self.value / other.value))
+        raise NotImplementedError("Left as excercise to the reader")
