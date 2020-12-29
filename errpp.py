4d16264a072dfdb446aa20a2860f260c33e8d773
0entropie <76582928+0entropie@users.noreply.github.com>
2020-12-29 22:57:40 +0100

    errpp.py: Rewrite to better separate the actual Data and Behavior.
    All Data (the value, absolute and relative error) is now stored in the
    ValueWithError class. The ErrorPropagation is handled separately by
    behavior-only classes.

--- errpp.py
+++ errpp.py
@@ -1,5 +1,7 @@
 import abc
 import math
+from numbers import Number
+import operator
 
 __percent_scale_factor = 100
 __ppm_scale_factor = 1_000_000
@@ -21,66 +23,44 @@ def abs_to_rel_err(value, abs_err):
 def percent_to_decimal(per):
     return per / __percent_scale_factor
 
+
+def _binary_arithmetic_op(method):
+    def perform_arithmetic_op(left, right):
+        if not left.prop.is_compatible(other=right.prop):
+            raise ValueError("Incompatible propagation methods")
+        # Call original method, only for division to check dividend basically..
+        method(left, right)
+        
+        operation = left._op_map[method.__name__]
+        new_val = operation(left.value, right.value)
+
+        prop_method = left._prop_map[operation]
+        new_abs_err = prop_method(new_val, left, right)
+
+        return ValueWithError.from_val_abs_err_pair(new_val, new_abs_err, left.prop)
+
+    return perform_arithmetic_op
+
 class ValueWithError:
-    def __init__(self, value, error):
+
+    def __init__(self, value, abs_err, rel_err, prop_method):
+        if not (isinstance(value, Number) or isinstance(abs_err, Number))\
+                or (rel_err is not None and not isinstance(rel_err, Number)):
+            raise TypeError("Value and Errors need to be numeric types")
         self.value = value
-        if not isinstance(error, BaseError):
-            raise TypeError(f"Expected Error Object but got {type(error)}")
-        self.error = error  # Error Object not number
-
-    def get_error(self):
-        return self.error
-
-    def __add__(self, other):
-        new_val = self.value + other.value
-        return ValueWithError(new_val, self.error.update_err_add(new_val, other.error))
-
-    def __sub__(self, other):
-        new_val = self.value - other.value
-        return ValueWithError(new_val, self.error.update_err_sub(new_val, other.error))
-
-    def __mul__(self, other):
-        new_val = self.value * other.value
-        if new_val == 0:
-            ref_val = max(self.value, other.value)
-        else:
-            ref_val = new_val
-        return ValueWithError(new_val, self.error.update_err_mul(ref_val, other.error))
-
-    def __truediv__(self, other):
-        if other.value == 0:
-            raise ZeroDivisionError("Attempt to divide by 0 Value")
-        new_val = self.value / other.value
-
-        if new_val == 0:
-            ref_val = other.value
-        else:
-            ref_val = new_val
-
-        return ValueWithError(new_val, self.error.update_err_div(ref_val, other.error))
-
-    def __neg__(self):
-        return ValueWithError(-self.value, self.error)
-
-    def __repr__(self):
-        try:
-            rel_error = "[{0:.3f}%]".format(self.error.get_percent_err())
-        except ValueError:
-            rel_error = "--%"
-        return "{0:.3f} \u00B1 {1:.3f} {2}".format(self.value,
-                                                   self.error.abs_err,
-                                                   rel_error)
-
-    def __str__(self):
-        return self.__repr__()
-
-class BaseError(abc.ABC):
-    def __init__(self, abs_err, rel_err):
         self.__abs_err = abs(abs_err)
-        if rel_err is not None:
-            self.__rel_err = abs(rel_err)
-        else:
-            self.__rel_err = None
+        self.__rel_err = abs(rel_err) if rel_err is not None else None
+        if prop_method is None:
+            raise ValueError("Error propagation Method may not be 'None'")
+        self.prop = prop_method
+        self._prop_map = {operator.add: prop_method.propagate_error_add,
+                          operator.sub: prop_method.propagate_error_sub, 
+                          operator.mul: prop_method.propagate_error_mul,
+                          operator.truediv: prop_method.propagate_error_div}
+        self._op_map = {'__add__': operator.add,
+                        '__sub__': operator.sub,
+                        '__mul__': operator.mul,
+                        '__truediv__': operator.truediv}
 
     @property
     def abs_err(self):
@@ -89,6 +69,57 @@ class BaseError(abc.ABC):
     @property
     def rel_err(self):
         return self.__rel_err
+
+    @classmethod
+    def from_val_abs_err_pair(cls, val, abs_err, prop_method):
+        rel_err = abs_err/val if val != 0 else None
+        return ValueWithError(val, abs_err, rel_err, prop_method)
+
+    @classmethod
+    def from_val_rel_err_pair(cls, val, rel_err, prop_method):
+        if rel_err is None:
+            raise ValueError("Can not construct ValueWithError from value and invalid relative Error")
+        abs_err = val * rel_err
+        return ValueWithError(val, abs_err, rel_err, prop_method)
+
+    def get_errors(self):
+        return (self.abs_err, self.rel_err)
+
+    def __value_with_same_prop(self, val, abs_err, rel_err):
+        return ValueWithError(val, abs_err, rel_err, self.prop)
+
+    def __compatibility_check_decorator(method, left, right):
+        def add_compatiblity_check(left, right):
+            if not left.prop.is_compatible(right.prop):
+                raise ValueError("Incompatible propagation methods")
+            method(left, right)
+
+    @_binary_arithmetic_op
+    def __add__(self, other):
+        pass
+
+    @_binary_arithmetic_op
+    def __sub__(self, other):
+        pass
+
+    @_binary_arithmetic_op
+    def __mul__(self, other):
+        pass
+
+    @_binary_arithmetic_op
+    def __truediv__(self, other):
+        if other.value == 0:
+            raise ZeroDivisionError("Attempt to divide by 0 Value")
+
+    def __neg__(self):
+        return self.__value_with_same_prop(-self.value, self.abs_err, self.rel_err)
+
+    def __repr__(self):
+        return "{0:.3f} \u00B1 {1:.3f} {2}".format(self.value,
+                                                   self.abs_err,
+                                                   self.rel_err)
+
+    __str__ = __repr__
 
     def get_percent_err(self):
         if self.__rel_err is not None:
@@ -102,131 +133,91 @@ class BaseError(abc.ABC):
         else:
             raise ValueError("Relative Error is not defined")
 
-    def type_check(self, other):
-        if not isinstance(other, self.__class__):
-            raise TypeError("Different error types: ", type(self), type(other))
-
-    def calculate_rel_err_from_abs(self, ref_val, abs_err):
-        if ref_val == 0:
-            return None
-        else:
-            return abs(abs_err / ref_val)
+class ErrorPropagationMethod(abc.ABC):
 
     @abc.abstractmethod
-    def update_err_add(self, ref_val, other):
+    def propagate_error_add(self, add_result, left, right):
         pass
 
     @abc.abstractmethod
-    def update_err_sub(self, ref_val, other):
+    def propagate_error_sub(self, sub_result, left, right):
         pass
 
     @abc.abstractmethod
-    def update_err_mul(self, ref_val, other):
+    def propagate_error_mul(self, mul_result, left, right):
         pass
 
     @abc.abstractmethod
-    def update_err_div(self, ref_val, other):
+    def propagate_error_div(self, div_result, left, right):
         pass
 
-class StatisticalError(BaseError):
+    @abc.abstractmethod
+    def is_compatible(self, other):
+        pass
+
+class StatisticalPropagation(ErrorPropagationMethod):
 
     # q = x+y , dq = sqrt( dx**2 + dy**2 )
-    def update_err_add(self, ref_val, other):
-        self.type_check(other)
-        new_abs_err = math.sqrt(self.abs_err**2 + other.abs_err**2)
-
-        return StatisticalError(new_abs_err, self.calculate_rel_err_from_abs(ref_val, new_abs_err))
+    def propagate_error_add(self, add_result, left_val, right_val):
+        return  math.sqrt(left_val.abs_err**2 + right_val.abs_err**2)
 
     # q = x-y , sqrt( dx**2 + dy**2 )
-    def update_err_sub(self, ref_val, other):
-        return self.update_err_add(ref_val, other)
+    def propagate_error_sub(self, sub_result, left, right):
+        return self.propagate_error_add(sub_result, left, right)
 
     # q = xy , dq = q * sqrt( (dx/x)**2 + (dy/y)**2 ) = sqrt( (dx*y)**2 + (dy*x)**2 )
-    def update_err_mul(self, ref_val, other):
-        self.type_check(other)
-
-        if self.rel_err is None:
-            new_abs_error = abs(ref_val * other.abs_err)
-            new_rel_error = None
-        elif other.rel_err is None:
-            new_abs_error = abs(ref_val * self.abs_err)
-            new_rel_error = None
-        else:
-            new_rel_error = math.sqrt(self.rel_err**2 + other.rel_err**2)
-            new_abs_error = abs(ref_val * new_rel_error)
-        return StatisticalError(new_abs_error, new_rel_error)
+    def propagate_error_mul(self, mul_result, left, right):
+        return math.sqrt((left.abs_err * right.value)**2 + (right.abs_err * left.value)**2)
 
     # q = x/y , dq = q * sqrt( (dx/x)**2 + (dy/y)**2 ) = sqrt( (dx/y)**2 + (dy*x/(y*y))**2 )
-    def update_err_div(self, ref_val, other):
-        return self.update_err_mul(ref_val, other)
+    def propagate_error_div(self, div_result, left, right):
+        return math.sqrt((left.abs_err / right.value)**2
+                         + (right.abs_err * left.value / right.value**2)**2)
 
-class WorstCaseError(BaseError):
+    def is_compatible(self, other):
+        return isinstance(other, self.__class__)
 
+
+class WorstCasePropogation(ErrorPropagationMethod):
     # q = x+y , dq = dx+dy
-    def update_err_add(self, ref_val, other):
-        self.type_check(other)
-        new_abs_error = self.abs_err + other.abs_err
-
-        return WorstCaseError(new_abs_error, self.calculate_rel_err_from_abs(ref_val, new_abs_error))
+    def propagate_error_add(self, add_result, left_val, right_val):
+        return  left_val.abs_err + right_val.abs_err
 
     # q = x-y , dq = dx+dy
-    def update_err_sub(self, ref_val, other):
-        return self.update_err_add(ref_val, other)
+    def propagate_error_sub(self, sub_result, left, right):
+        return self.propagate_error_add(sub_result, left, right)
 
     # q = xy , dq = |q|*(dx/|x| + dy/|y|) = dx*|y| + dy*|x|
-    def update_err_mul(self, ref_val, other):
-        self.type_check(other)
+    def propagate_error_mul(self, mul_result, left, right):
+        return left.abs_err * abs(right.value) + right.abs_err * abs(left.value)
 
-        if self.rel_err is None:
-            new_abs_error = abs(ref_val * other.abs_err)
-            new_rel_error = None
-        elif other.rel_err is None:
-            new_abs_error = abs(ref_val * self.abs_err)
-            new_rel_error = None
-        else:
-            new_rel_error = self.rel_err + other.rel_err
-            new_abs_error = abs(new_rel_error * ref_val)
-        return WorstCaseError(new_abs_error, new_rel_error)
+    # q = x/y , dq = |q|*(dx/|x| + dy/|y|) = dx/|y| + dy*|x|/|y*y| = dx*|y| + dy*|x|
+    def propagate_error_div(self, div_result, left, right):
+        return left.abs_err / abs(right.value) + right.abs_err * abs(left.value) / right.value**2
 
-    # q = x/y , dq = |q|*(dx/|x| + dy/|y|) = dx/|y| + dy*|x|/|y*y|
-    def update_err_div(self, ref_val, other):
-        return self.update_err_mul(ref_val, other)
+    def is_compatible(self, other):
+        return isinstance(other, self.__class__)
 
 
-class ExtremeError(BaseError):
-
+class ExtremePropagation(ErrorPropagationMethod):
     # q = x+y , dq = dx+dy
-    def update_err_add(self, ref_val, other):
-        self.type_check(other)
-        new_abs_error = self.abs_err + other.abs_err
-
-        return ExtremeError(new_abs_error, self.calculate_rel_err_from_abs(ref_val, new_abs_error))
+    def propagate_error_add(self, add_result, left_val, right_val):
+        return  left_val.abs_err + right_val.abs_err
 
     # q = x-y , dq = dx+dy
-    def update_err_sub(self, ref_val, other):
-        return self.update_err_add(ref_val, other)
+    def propagate_error_sub(self, sub_result, left, right):
+        return self.propagate_error_add(sub_result, left, right)
 
     # q = xy , dq = |q|*(dx/|x| + dy/|y| + (dx/x)*(dy/y)) = dx*|y| + dy*|x| + dx*dy
-    def update_err_mul(self, ref_val, other):
-        self.type_check(other)
-
-        if self.rel_err is None:
-            new_abs_error = abs(ref_val * other.abs_err + self.abs_to_rel_err * other.abs_err)
-            new_rel_error = None
-        elif other.rel_err is None:
-            new_abs_error = abs(ref_val * self.abs_err + self.abs_to_rel_err * other.abs_err)
-            new_rel_error = None
-        else:
-            new_rel_error = self.rel_err + other.rel_err + self.rel_err * other.rel_err
-            new_abs_error = abs(new_rel_error * ref_val)
-
-        return ExtremeError(new_abs_error, new_rel_error)
+    def propagate_error_mul(self, mul_result, left, right):
+        return left.abs_err * abs(right.value)\
+               + right.abs_err * abs(left.value)\
+               + left.abs_err * right.abs_err
 
     # q = x/y , q(1 + dq/|q|) = x(1 + dx/|x|) / (y(1 + dy/|y|)) ,, dq = (|x| + dx) / (|y| - dy) - |x/y|
-    def update_err_div(self, ref_val, other):
-        #self.type_check(other)
-        #self.zeroDiv_check(other)
-        #return ExtremeError(
-        #    self.value / other.value, (abs(self.value) + self.error) /
-        #    (abs(other.value) - other.error) - abs(self.value / other.value))
-        raise NotImplementedError("Left as excercise to the reader")
+    def propagate_error_div(self, div_result, left, right):
+        return (abs(left.value) + left.abs_err) / (abs(right.value) - right.abs_err)\
+                - abs(div_result)
+
+    def is_compatible(self, other):
+        return isinstance(other, self.__class__)
